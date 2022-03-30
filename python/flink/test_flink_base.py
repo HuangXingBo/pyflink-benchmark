2d5d9ced31ffa96b2127abaf82b73b3e30bd97fb
huangxingbo <hxbks2ks@gmail.com>
2022-03-30 16:06:18 +0800

    fix

--- python/flink/test_flink_base.py
+++ python/flink/test_flink_base.py
@@ -1,7 +1,7 @@
 from abc import ABC
 
 from pyflink.datastream import StreamExecutionEnvironment, RuntimeExecutionMode, CheckpointingMode
-from pyflink.table import TableEnvironment, EnvironmentSettings
+from pyflink.table import TableEnvironment, EnvironmentSettings, StreamTableEnvironment
 
 from python.test_case_utils import BenchmarkTestBase
 
@@ -10,10 +10,9 @@ class PyFlinkTests(BenchmarkTestBase, ABC):
     def __init__(self):
         self.batch_env = StreamExecutionEnvironment.get_execution_environment()
         self.batch_env.set_runtime_mode(RuntimeExecutionMode.BATCH)
-        self.batch_env.enable_checkpointing(30_000, CheckpointingMode.EXACTLY_ONCE)
         self.batch_table_env = TableEnvironment.create(EnvironmentSettings.in_batch_mode())
 
         self.stream_env = StreamExecutionEnvironment.get_execution_environment()
         self.stream_env.set_runtime_mode(RuntimeExecutionMode.STREAMING)
         self.stream_env.enable_checkpointing(30_000, CheckpointingMode.EXACTLY_ONCE)
-        self.stream_table_env = TableEnvironment.create(EnvironmentSettings.in_streaming_mode())
+        self.stream_table_env = StreamTableEnvironment.create(self.stream_env)
