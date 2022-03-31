4a928c0793a80f2fbae31d2f4b6e108cbab06a69
huangxingbo <hxbks2ks@gmail.com>
2022-03-31 16:28:27 +0800

    disable chain

--- python/flink/test_flink_base.py
+++ python/flink/test_flink_base.py
@@ -1,7 +1,7 @@
 from abc import ABC
 
 from pyflink.datastream import StreamExecutionEnvironment, RuntimeExecutionMode, CheckpointingMode
-from pyflink.table import TableEnvironment, EnvironmentSettings, StreamTableEnvironment
+from pyflink.table import StreamTableEnvironment
 
 from python.test_case_utils import BenchmarkTestBase
 
@@ -10,9 +10,12 @@ class PyFlinkTests(BenchmarkTestBase, ABC):
     def __init__(self):
         self.batch_env = StreamExecutionEnvironment.get_execution_environment()
         self.batch_env.set_runtime_mode(RuntimeExecutionMode.BATCH)
-        self.batch_table_env = TableEnvironment.create(EnvironmentSettings.in_batch_mode())
+        self.batch_env.enable_checkpointing(60_000, CheckpointingMode.EXACTLY_ONCE)
+        self.batch_env.disable_operator_chaining()
+        self.batch_table_env = StreamTableEnvironment.create(self.batch_env)
 
         self.stream_env = StreamExecutionEnvironment.get_execution_environment()
         self.stream_env.set_runtime_mode(RuntimeExecutionMode.STREAMING)
-        self.stream_env.enable_checkpointing(30_000, CheckpointingMode.EXACTLY_ONCE)
+        self.stream_env.enable_checkpointing(60_000, CheckpointingMode.EXACTLY_ONCE)
+        self.stream_env.disable_operator_chaining()
         self.stream_table_env = StreamTableEnvironment.create(self.stream_env)
